5df15e144a3a8e68621753b78c1b4474c1fe300e
Artur Siedlecki <asiedlecki@protonmail.com>
2021-09-01 08:18:35 +0200

    Union with multiple items.

--- Dictionaries and sets/set_union.py
+++ Dictionaries and sets/set_union.py
@@ -14,9 +14,14 @@ from prescription_data import adverse_interactions
 
 meds_to_watch = set()
 
-for interaction in adverse_interactions:
-    # meds_to_watch = meds_to_watch.union(interaction)
-    # meds_to_watch.update(interaction)
-    ## this operator also updates set
-    meds_to_watch |= interaction
+# for interaction in adverse_interactions:
+#     # meds_to_watch = meds_to_watch.union(interaction)
+#     # meds_to_watch.update(interaction)
+#     ## this operator also updates set
+#     meds_to_watch |= interaction
+
+# multiple items can be passed to the method
+meds_to_watch.update(*adverse_interactions)
+
 print(sorted(meds_to_watch))
+print(*sorted(meds_to_watch), sep='\n')
