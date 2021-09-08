a544f9a21b0006abb53836217cea3669677e2bc4
Artur Siedlecki <asiedlecki@protonmail.com>
2021-09-09 00:23:29 +0200

    Manipulating data with shelve.

--- Python IO/shelve_example.py
+++ Python IO/shelve_example.py
@@ -19,8 +19,19 @@ fruit['lemon'] = "a sour, yellow citrus fruit"
 fruit['grape'] = "a small, sweet fruit growing in bunches"
 fruit['lime'] = "a sour, green citrus fruit"
 
-print(fruit["lemon"])
-print(fruit["grape"])
-fruit.close()
+# print(fruit["lemon"])
+# print(fruit["grape"])
+#
+# fruit["lime"] = "great with tequila"
+#
+# for snack in fruit:
+#     print(snack + ": " + fruit[snack])
 
-print(fruit)
+while True:
+    dict_key = input("Please enter a fruit: ")
+    if dict_key == "quit":
+        break
+    description = fruit.get(dict_key, "We don't have a " + dict_key)
+    print(description)
+
+fruit.close()
